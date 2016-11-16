65adf91fb4c4eb7bee520f8f8c3652c706f5725e
Alfie <alfie_ab@hotmail.co.uk>
2016-11-16 15:10:23 +0000

    photos now saving in database

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,27 @@ class PhotosController < ApplicationController
   def index
   end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    if @photo.save
+      redirect_to photos_path
+    else
+      redirect_to new_photo_path
+    end
+  end
+
+  def show
+  end
+
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :caption)
+  end
+
 end
