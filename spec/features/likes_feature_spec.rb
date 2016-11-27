93c22ba3eed995269e77c6e8cc516769d87b797d
Alfie <alfie_ab@hotmail.co.uk>
2016-11-27 17:03:24 +0000

    testing for likes added

---
+++ spec/features/likes_feature_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'liking a photo' do
+
+  it 'should allow you to like a picture' do
+    post_picture('picture')
+    click_link('picture')
+    click_button('Like!')
+    expect(page).to have_content('1 Like')
+  end
+
+end
