7b0ba55b6b39d3f3f76c6b19305838ca4dbe6f27
Alfie <alfie_ab@hotmail.co.uk>
2016-11-29 17:12:56 +0000

    continued work on likes, params coming up as empty however

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,14 @@
 class LikesController < ApplicationController
 
   def create
+    @like = Like.create(like_params)
+    redirect_to photos_path
   end
-  
+
+  private
+
+  def like_params
+    params.require(:like).permit(:photo_id)
+  end
+
 end
--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -5,7 +5,7 @@ feature 'liking a photo' do
   it 'should allow you to like a picture' do
     post_picture('picture')
     click_link('picture')
-    click_button('Save Like')
+    click_button('Like!')
     visit '/'
     expect(page).to have_content('1 Like')
   end
